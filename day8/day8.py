2faab1a0438c490df4585344f6cf59e3936a31c5
Lucas <lucaspahlke@gmx.de>
2022-12-13 20:47:40 +0100

    changed layout for solution

---
+++ day8/day8.py
@@ -0,0 +1,17 @@
+def solve_part_1():
+    with open('puzzle_input.txt', 'r') as f:
+    datastream = f.readline()
+
+def solve_part_2():
+    with open('puzzle_input.txt', 'r') as f:
+    datastream = f.readline()
+
+
+if __name__ == "__main__":
+    # returns 
+    solution1 = solve_part_1()
+    print(solution1)
+
+    # returns 
+    solution2 = solve_part_2()
+    print(solution2)
