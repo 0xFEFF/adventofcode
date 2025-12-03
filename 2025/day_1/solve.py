c907fefd4bdf4b3b76b46f5d57df933fc77dd70a
0xFEFF <44374572+0xFEFF@users.noreply.github.com>
2025-12-03 08:18:14 +0000

    added solution for part two day one

--- 2025/day_1/solve.py
+++ 2025/day_1/solve.py
@@ -9,42 +9,76 @@ def get_lock_number(data: list[str]) -> int:
     for line in data:
         letter = line[0]
         number = int(line[1:])
-        print(f"Letter: {letter}, Number: {number}")
+        #print(f"Letter: {letter}, Number: {number}")
         
         # everything above 100 can be ignored since 100 is a full rotation
         if number > 100:
             number = number % 100
-            print(f"Adjusted number for three digits: {number}")
+            #print(f"Adjusted number for three digits: {number}")
 
         if letter == 'R':
             index += number
         elif letter == 'L':
             index -= number
 
-        print(f"Current index: {index}")
-
-
+        #print(f"current index: {index}")
+        
         if index < 0:
             index = len(lock_number) - abs(index)
         if index >= len(lock_number):
             index = index % len(lock_number)
         
-        print(f"fixed index: {index}")
+        #print(f"fixed index: {index} and number: {lock_number[index]}")
 
         if lock_number[index] == 0:
             zeros += 1
 
-    print(f"Total zeros encountered: {zeros}")
-    return zeros
+    print(f"Total zeros encountered at the end of rotation for part one: {zeros}")
+
+def get_lock_number_part_two(data: list[str]) -> int:
+    index = 0
+    zeros = 0
+
+    for line in data:
+        letter = line[0]
+        number = int(line[1:])
+        #print(f"Letter: {letter}, Number: {number}")
+
+        for step in range(1,number+1):
+            if letter == 'R':
+                index = index + 1
+            elif letter == 'L':
+                index = index - 1
+
+            #print(f"current step: {step}, current index: {index}")
+
+            if index < 0:
+                index = len(lock_number) - abs(index)
+
+            if index >= len(lock_number):
+                index = index % len(lock_number)
+
+            if lock_number[index] == 0:
+                zeros += 1
+                #print(f"current index: {index} and number: {lock_number[index]}")
+
+    print(f"Total zeros encountered at the end of rotation for part two: {zeros}")
+
+
 
 if __name__ == '__main__':
     lock_number = list(range(50, 100)) + list(range(0, 50))
-    print(lock_number)
 
-    test_data = ['R3', 'L55', 'R2', 'R125']
-    zeros = get_lock_number(test_data)
-    print(f"number of zeros: {zeros}")
+    #test_data = ['R3', 'L55', 'R2', 'R125']
+    #get_lock_number(test_data)
 
+    # 1031 is the result
     data = read_input('input.txt')
-    zeros = get_lock_number(data)
-    print(f"number of zeros: {zeros}")
+    get_lock_number(data)
+
+    #test_data_rotation = read_input('example.txt')
+    #get_lock_number_part_two(test_data_rotation)
+
+    # 5831 is the result
+    data = read_input('input.txt')
+    get_lock_number_part_two(data)
