bbec67a68633802f2c3592c9b3afb6f5b8be5827
DeLuc <l@noreply.hey>
2025-12-06 19:46:45 +0100

    add solution day three part two

--- 2025/day_3/day3.py
+++ 2025/day_3/day3.py
@@ -1,4 +1,4 @@
-def read_input(file_path):
+def read_input(file_path: str) -> list[str]:
     with open(file_path, 'r') as file:
         data = file.read().strip().split('\n')
     return data
@@ -19,7 +19,26 @@ def solution_part_one(data: list[str]):
 
 
 def solution_part_two(data: list[str]):
-    pass
+    jolt_max = 0
+    for bank in data:
+        joltage = ''
+        current_index = 0
+        while len(joltage) < 12:
+            highest_value = 0
+            # it is necessary to find the next 11 values that are the highest
+            for j in range(current_index,len(bank)):
+                if int(bank[j]) > highest_value and len(bank[j:]) + len(joltage) > 11:
+                    highest_value = int(bank[j])
+                    current_index = j + 1
+                    #print(bank[j], j)
+            
+            joltage += str(highest_value)
+            
+        print(f"jolt {joltage}")
+        jolt_max += int(joltage)
+
+    print(f"jolt max {jolt_max}")
+
 
 if __name__ == "__main__":
     print("Day 3: Lobby")
@@ -27,9 +46,11 @@ if __name__ == "__main__":
     example_data = read_input('example.txt')
     #solution_part_one(['818181911112111']) 
     #print(example_data)
-    solution_part_one(example_data) 
+    #solution_part_one(example_data) 
+
+    solution_part_two(['987654321111111'])
     solution_part_two(example_data)
 
     input_data = read_input('input.txt')
-    solution_part_one(input_data)
-    #solution_part_two(input_data)
+    #solution_part_one(input_data)
+    solution_part_two(input_data)
