3ca030a3a7b29033701a43e0b5b4cdc1a5c5963d
DeLuc <l@noreply.hey>
2025-12-07 15:20:54 +0100

    rework template file to make it more flexible

--- template.py
+++ template.py
@@ -1,7 +1,17 @@
 def read_input(file_path: str) -> list[str]:
+    """
+    Reads the input file and returns a list of strings.
+    If the file contains multiple lines, splits by newline.
+    If the file contains a single line, splits by comma.
+    """
     with open(file_path, 'r') as file:
-        data = file.read().strip().split(',')
-    return data
+        data = file.read().strip()
+    if '\n' in data:
+        return data.split('\n')
+    elif ',' in data:
+        return data.split(',')
+    else:
+        raise Exception("delimiter not known, pls check the input data") 
 
 def solution_part_one(data: list[str]):
     pass
@@ -10,7 +20,7 @@ def solution_part_two(data: list[str]):
     pass
 
 if __name__ == "__main__":
-    print("Day 2: Gift Shop")
+    print("Day X: ")
 
     example_data = read_input('example.txt')
     solution_part_one(example_data) 
