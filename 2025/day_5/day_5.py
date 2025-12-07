0aadd4a9b25c86006c798198e35fe2aa6f76645b
DeLuc <l@noreply.hey>
2025-12-07 18:44:50 +0100

    wip: solve day five part two

---
+++ 2025/day_5/day_5.py
@@ -0,0 +1,48 @@
+def read_input(file_path: str) -> tuple[list[str], list[str]]:
+    with open(file_path, 'r') as file:
+        data = file.read().strip()
+    data = data.split("\n\n")
+    return data[0].split('\n'), data[1].split("\n")
+
+def check_id_in_ids(id: str, ids: list[str]) -> bool:
+    print(id)
+    for i in range(len(ids)):
+        start, end = map(int, ids[i].split("-"))
+        print(range(start, end+1))
+        if int(id) in range(start, end+1):
+            return True
+    return False
+
+def solution_part_one(ids: list[str], ingredients: list[str]):
+    fresh_items = 0
+    
+    for ingredient in ingredients:
+        if check_id_in_ids(ingredient, ids):
+            fresh_items += 1
+
+    print(f"fresh items amount {fresh_items}")
+
+
+def solution_part_two(ids: list[str]):
+    amount_ids = []
+
+    for id_range in ids:
+        start, end = map(int, id_range.split("-"))
+        print(start, end, end - start)
+        for i in range(start, end +1):
+            if i not in amount_ids:
+                amount_ids.append(i)
+
+    print(f"amount valid ids {len(amount_ids)}")
+
+
+if __name__ == "__main__":
+    print("Day 5: Cafeteria")
+
+    id_ranges, ingredients = read_input('example.txt')
+    #solution_part_one(id_ranges, ingredients) 
+    solution_part_two(id_ranges)
+
+    id_ranges, ingredients = read_input('input.txt')
+    #solution_part_one(id_ranges, ingredients)
+    #solution_part_two(id_ranges)
