149211e66e0e286f64dd2b410dc2a2f2d29dfcd3
0xFEFF <44374572+0xFEFF@users.noreply.github.com>
2025-12-17 09:42:11 +0000

    add initial solution try

---
+++ 2025/day_9/day9.py
@@ -0,0 +1,26 @@
+def read_input(file_path: str) -> list[str]:
+    with open(file_path, 'r') as file:
+        data = file.read().strip()
+    if '\n' in data:
+        return data.splitlines()
+    elif ',' in data:
+        return data.split(',')
+    else:
+        raise Exception("delimiter not known, pls check the input data") 
+
+def solution_part_one(data: list[str]):
+    pass
+
+def solution_part_two(data: list[str]):
+    pass
+
+if __name__ == "__main__":
+    print("Day 9: Movie Theater")
+
+    example_data = read_input('example.txt')
+    solution_part_one(example_data) 
+    #solution_part_two(example_data)
+
+    #input_data = read_input('input.txt')
+    #solution_part_one(input_data)
+    #solution_part_two(input_data)
