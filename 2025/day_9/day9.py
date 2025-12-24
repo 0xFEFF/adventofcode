b861c710984bf345b9050b0da33f39afdff31245
DeLuc <l@noreply.hey>
2025-12-24 20:55:15 +0100

    add solution part one day nine

--- 2025/day_9/day9.py
+++ 2025/day_9/day9.py
@@ -1,15 +1,25 @@
-def read_input(file_path: str) -> list[str]:
+def read_input(file_path: str) -> list[tuple[int, ...]]:
     with open(file_path, 'r') as file:
         data = file.read().strip()
-    if '\n' in data:
-        return data.splitlines()
-    elif ',' in data:
-        return data.split(',')
-    else:
-        raise Exception("delimiter not known, pls check the input data") 
+    data = data.splitlines()
+    data = [tuple(map(int, line.split(','))) for line in data]
+    return data
 
-def solution_part_one(data: list[str]):
-    pass
+def calc_rect(p1: tuple[int, ...], p2: tuple[int, ...]):
+    #print(p1, p2, abs(p1[0] - p2[0]), abs(p1[1] - p2[1]), abs(p1[0] - p2[0]) * abs(p1[1] - p2[1]))
+    return (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
+
+def solution_part_one(data: list[tuple[int, ...]]):
+    max_size = 0
+    for i in range(len(data)):
+        for j in range(i+1, len(data)):
+            # calculate rectangle
+            rect_size = calc_rect(data[i], data[j])
+            # check if > max size
+            if rect_size > max_size:
+                max_size = rect_size
+    
+    print(max_size)
 
 def solution_part_two(data: list[str]):
     pass
@@ -18,9 +28,10 @@ if __name__ == "__main__":
     print("Day 9: Movie Theater")
 
     example_data = read_input('example.txt')
+    print(sorted(example_data))
     solution_part_one(example_data) 
     #solution_part_two(example_data)
 
-    #input_data = read_input('input.txt')
+    input_data = read_input('input.txt')
+    #print(sorted(input_data))
     #solution_part_one(input_data)
-    #solution_part_two(input_data)
